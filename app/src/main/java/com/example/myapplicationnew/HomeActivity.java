8ede983ce2e14188fa3c7dea296cf3d8d3816f9c
AnujVicky <anujvikcy801@gmail.com>
2020-07-08 14:14:48 +0530

    First Commit

--- app/src/main/java/com/example/myapplicationnew/HomeActivity.java
+++ app/src/main/java/com/example/myapplicationnew/HomeActivity.java
@@ -1,131 +1,14 @@
 package com.example.myapplicationnew;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.storage.FileDownloadTask;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.zip.Inflater;
-
-public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
-    private Toolbar toolbar;
-    private  ImageView  profileImage;
-    private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
-    final String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-    final String phoneNumber = FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber();
-    StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("images/"+userId);
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        toolbar = findViewById(R.id.main_toolbar);
-        setSupportActionBar(toolbar);
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.nav_view);
-
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
-                this,
-                drawerLayout,
-                toolbar,
-                R.string.openNavDrawer,
-                R.string.closeNavDrawer
-        );
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
-        navigationView.setNavigationItemSelectedListener(this);
-        super.onStart();
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_setting)
-        {
-            Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
-            startActivity(intent);
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
-    }
-
-    @Override
-     protected void onStart() {
-        super.onStart();
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        View hView =  navigationView.getHeaderView(0);
-        profileImage =(ImageView)hView.findViewById(R.id.navProfileImage);
-        final TextView navName = (TextView)hView.findViewById(R.id.navName);
-        final TextView navPhone = (TextView)hView.findViewById(R.id.navNumber);
-        navPhone.setText(phoneNumber);
-        try {
-            final File localFile = File.createTempFile("profileImg","jpg");
-            storageRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                    Bitmap bitMapNew = BitmapFactory.decodeFile(localFile.getAbsolutePath());
-                    profileImage.setImageBitmap(bitMapNew);
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        DocumentReference docRef = FirebaseFirestore.getInstance().collection("user").document(userId);
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot docSnap = task.getResult();
-                    if (docSnap.exists()) {
-                        navName.setText(docSnap.get("name").toString());
-                        navPhone.setText(docSnap.get("phone_number").toString());
-                    } else {
-                        Log.d("Data", "No Data");
-                    }
-                }
-            }
-        });
-
     }
 }
